f3c9e1d89791093bd41b2525754193e62adccdee
EkaterynaRuslanovna <gorgona3005@gmail.com>
2023-05-13 13:32:46 +0300

    fixed translate

--- Lesson_9/task_2.py
+++ Lesson_9/task_2.py
@@ -20,4 +20,4 @@ def find_manager(name_of_file):
             top_manager_name = manager_object["manager"]["first_name"]
             top_manager_surname = manager_object["manager"]["last_name"]
 
-    print(f"Імʼя: {top_manager_name}, Фамілія: {top_manager_surname}, Загальна сума продажів: {total}")
+    print(f"Імʼя: {top_manager_name}, Прізвище: {top_manager_surname}, Загальна сума продажів: {total}")
