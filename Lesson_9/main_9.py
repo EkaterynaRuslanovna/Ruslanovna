13a76b7681d9dfb6f9c671e836bb2b61c9f8444e
EkaterynaRuslanovna <gorgona3005@gmail.com>
2023-05-13 09:49:00 +0300

    fixed HW 9

--- Lesson_9/main_9.py
+++ Lesson_9/main_9.py
@@ -1,21 +1,11 @@
-import os
-from Lesson_7 import Task_1
 from Task_1 import find_droup
 from Task_2 import find_manager
 
 
 if __name__ == '__main__':
     print("---------------------------------------------Завдання 1---------------------------------------------")
-    """Знаходимо шлях до файлу і змінюємо директорію"""
 
-    os.chdir("/Users/macbookpro/downloaded")
     name_of_file = "group_people.json"
-    current_directory = os.getcwd()
-
-    file_path = Task_1.searching_file(name_of_file, current_directory)
-    file_path = Task_1.convert_to_true_path(file_path)
-
-    os.chdir(file_path)
 
     try:
         find_droup(name_of_file)
@@ -30,16 +20,7 @@ if __name__ == '__main__':
 
     print("---------------------------------------------Завдання 2---------------------------------------------")
 
-    """Знаходимо шлях до файлу і змінюємо директорію"""
-
-    os.chdir("/Users/macbookpro/downloaded")
     name_of_file = "manager_sales.json"
-    current_directory = os.getcwd()
-
-    file_path = Task_1.searching_file(name_of_file, current_directory)
-    file_path = Task_1.convert_to_true_path(file_path)
-
-    os.chdir(file_path)
 
     try:
         find_manager(name_of_file)
